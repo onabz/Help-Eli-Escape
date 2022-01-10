60d04922eb6ad22918e773a11ba73e281d380d48
onabz <ONABOLUMBE@GMAIL.COM>
2022-01-10 09:12:23 +0000

    Add heading and some intro

--- run.py
+++ run.py
@@ -1,1 +1,16 @@
 
+
+
+
+print()
+print()
+print("    #####################")
+print("    #                   #")
+print("    #  Help Eli Escape  #")
+print("    #                   #")
+print("    #####################")
+print()
+print()
+print("Eli just robbed the Central Bank of the world.")
+print("Eli is trying to escape with large bags of cash.")
+print()
