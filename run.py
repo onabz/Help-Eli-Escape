f380e6735a5f841768e2ee675d3a283e6c013e25
onabz <ONABOLUMBE@GMAIL.COM>
2022-01-10 09:33:52 +0000

    Add input and if statements to start or end game

--- run.py
+++ run.py
@@ -14,3 +14,8 @@ print()
 print("Eli just robbed the Central Bank of the world.")
 print("Eli is trying to escape with large bags of cash.")
 print()
+help_Eli = input("Would you like to help Eli escape from the police? (Y / N): ")
+if help_Eli == 'n' or help_Eli == 'N':
+    print("Eli won't be getting any help this time")
+elif help_Eli == 'y' or help_Eli == 'Y':
+    Begin_Game()
