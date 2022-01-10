bd924261a75e4c51d16a5dc24348a19db5505571
onabz <ONABOLUMBE@GMAIL.COM>
2022-01-10 11:41:18 +0000

    Define begin game function

--- run.py
+++ run.py
@@ -1,6 +1,32 @@
 
 
 
+def begin_Game():
+    print()
+    print("Eli just robbed the Central Bank of the world.")
+    print("Eli is trying to escape with large bags of cash.")
+    print("The police have been alerted and are in hot pursuit.")
+    print("Help Eli escape from the police.")
+    print()
+    print("Eli storms out of the bank on to the busy road.")
+    print("He comes up to a fork at the end of the road.")
+    print("He is faced with two choices: ")
+    print()
+    print("Choice 1:  Turn left.")
+    print("Choice 2:  Turn right.")
+    print()
+    first_Choice = input("What choice do you want to make? ( 1 / 2 ): ")
+    if first_Choice == '1':
+        print()
+        choice1()
+    elif first_Choice == '2':
+        print()
+        choice2()
+
+
+    
+
+
 
 print()
 print()
@@ -18,4 +44,4 @@ help_Eli = input("Would you like to help Eli escape from the police? (Y / N): ")
 if help_Eli == 'n' or help_Eli == 'N':
     print("Eli won't be getting any help this time")
 elif help_Eli == 'y' or help_Eli == 'Y':
-    Begin_Game()
+    begin_Game()
