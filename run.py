0bb89d250c4d093ea630c0e5039e6b58bef1d51d
onabz <ONABOLUMBE@GMAIL.COM>
2022-01-13 19:26:44 +0000

    Fix formatting

--- run.py
+++ run.py
@@ -8,38 +8,30 @@ def clear():
 
 def begin_game():
     clear()
-    print()
-    print("Eli just robbed the Central Bank of the world.")
+    print("\nEli just robbed the Central Bank of the world.")
     print("Eli is trying to escape with large bags of cash.")
     print("The police have been alerted and are in hot pursuit.")
     print("Help Eli escape from the police.")
-    print()
-    print("Eli storms out of the bank on to the busy road.")
+    print("\nEli storms out of the bank on to the busy road.")
     print("He comes up to a fork at the end of the road.")
     print("He is faced with two choices: ")
-    print()
-    print("Choice 1:  Turn left.")
-    print("Choice 2:  Turn right.")
-    print()
+    print("\nChoice 1:  Turn left.")
+    print("Choice 2:  Turn right.\n")
     while True:
         first_choice = input("What choice do you want to make? ( 1 / 2 ):\n ")
         if first_choice not in ['1', '2']:
             print("That is not a valid option, please try again")
         else:
             if first_choice == '1':
-                print()
                 choice1()
                 break
             else:
-                if first_choice == '2':
-                    print()
-                    choice2()
-                    break
+                choice2()
+                break
 
 
 def restart_eli():
     clear()
-    print()
     while True:
         start_eli = input(
             "Would you like to try again? "
@@ -59,75 +51,58 @@ def restart_eli():
 
 def choice1():
     clear()
-    print()
-    print("He turns left to a narrow path that leads him up to a shed.")
+    print("\nHe turns left to a narrow path that leads him up to a shed.")
     print("He breaks open the doors to the shed.")
     print("He looks around for anything that can help him escape.")
     print("He finds an open safe that contains three keys: ")
-    print()
-    print("Choice 1:  keys to a motorbike.")
+    print("\nChoice 1:  keys to a motorbike.")
     print("Choice 2:  keys to a car.")
-    print("Choice 3:  keys to a boat.")
-    print()
+    print("Choice 3:  keys to a boat.\n")
     while True:
         second_choice = input("Pick a key ( 1 / 2 / 3 ):\n ")
         if second_choice not in ['1', '2', '3']:
             print("That is not a valid option, please try again")
         else:
             if second_choice == '1':
-                print()
                 choice1_1()
                 break
+            elif second_choice == '2':
+                choice1_2()
+                break
             else:
-                if second_choice == '2':
-                    print()
-                    choice1_2()
-                    break
-                else:
-                    if second_choice == '3':
-                        print()
-                        choice1_3()
-                        break
+                choice1_3()
+                break
 
 
 def choice1_1():
     clear()
-    print()
-    print("Eli takes the keys to the motorbike.")
+    print("\nEli takes the keys to the motorbike.")
     print("He starts the motorbike.")
     print("He keeps riding till he comes up to the main road.")
     print("The main road leads to 3 coloured dirt paths:")
-    print()
-    print("choice 1:  The brown dirt path")
+    print("\nchoice 1:  The brown dirt path")
     print("choice 2:  The black dirt path")
-    print("choice 3:  The grey dirt path")
-    print()
+    print("choice 3:  The grey dirt path\n")
     while True:
         third_choice = input("Choose a dirt path ( 1 / 2 / 3 ):\n ")
         if third_choice not in ['1', '2', '3']:
             print("That is not a valid option, please try again")
         else:
             if third_choice == '1':
-                print()
                 choice1_1a()
                 break
+            elif third_choice == '2':
+                choice1_1b()
+                break
             else:
-                if third_choice == '2':
-                    print()
-                    choice1_1b()
-                    break
-                else:
-                    if third_choice == '3':
-                        print()
-                        choice1_1c()
-                        break
+                choice1_1c()
+                break
 
 
 def choice1_1a():
     clear()
-    print()
     print(
-        "As Eli rides through the brown dirt path, "
+        "\nAs Eli rides through the brown dirt path, "
         "the motorbike starts to slow down.")
     print(
         "So he stops by a diner in the middle of no where, "
@@ -138,17 +113,14 @@ def choice1_1a():
     print("The police finds his motorbike but cannot find him")
     print("Eli is able escape with all the bags of cash intact.")
     print("Eli is now a very rich man living freely.")
-    print()
-    print("CONGRATULATIONS. YOU WIN !!!!!")
-    print()
-    input("Press enter to continue")
+    print("\nCONGRATULATIONS. YOU WIN !!!!!")
+    input("\nPress enter to continue")
     restart_eli()
 
 
 def choice1_1b():
     clear()
-    print()
-    print("As he rides through the black dirt path, Eli gets to a tunnel.")
+    print("\nAs he rides through the black dirt path, Eli gets to a tunnel.")
     print(
         "Unknown to him there are policemen waiting "
         "on the other side of the tunnel.")
@@ -158,10 +130,8 @@ def choice1_1b():
     print(
         "Eli falls to the ground with all his bags of cash "
         "and the police arrest him.")
-    print()
-    print("That is the end of the road for Eli.")
-    print()
-    print()
+    print("\nThat is the end of the road for Eli.")
+    input("\nPress enter to continue")
     restart_eli()
 
 
@@ -170,9 +140,8 @@ def choice1_1c():
     whshvjhcbvjhbvjdf
     """
     clear()
-    print()
     print(
-        "As he rides through the grey dirt path, "
+        "\nAs he rides through the grey dirt path, "
         "his bike starts to slow down.")
     print("He discovers that he is low on fuel.")
     print(
@@ -181,16 +150,14 @@ def choice1_1c():
     print("He is now on open land with no cover to hide under.")
     print("The police finally spot him.")
     print("He is arrested and all the stolen cash is recovered.")
-    print()
-    print()
+    input("\nPress enter to continue")
     restart_eli()
 
 
 def choice1_2():
     clear()
-    print()
     print(
-        "Eli takes the keys to the car,"
+        "\nEli takes the keys to the car,"
         "enters the car and starts driving.")
     print(
         "Not long after he starts driving he hears the police sirens "
@@ -198,68 +165,57 @@ def choice1_2():
     print(
         "As he drives up the main road, "
         " he comes to a path leading to 2 bridges: ")
-    print()
-    print("choice 1:  Bridge 1")
+    print("\nchoice 1:  Bridge 1")
     print("choice 2:  Bridge 2")
-    print()
     while True:
         fourth_choice = input("Choose which bridge to take ( 1 / 2 ):\n ")
         if fourth_choice not in ['1', '2']:
             print("That is not a valid option, please try again")
         else:
             if fourth_choice == '1':
-                print()
                 choice1_2a()
                 break
             else:
-                if fourth_choice == '2':
-                    print()
-                    choice1_2b()
-                    break
+                choice1_2b()
+                break
 
 
 def choice1_2a():
     clear()
-    print()
     print(
-        "As Eli drives through bridge 1, "
+        "\nAs Eli drives through bridge 1, "
         "the car starts to skid and then overturns.")
     print(
         "He climbs out of the car and starts to run across the bridge, "
         "but the bridge is broken half way.")
     print("The police surrounds him and he is arrested.")
-    print()
-    print()
-    begin_game()
+    input("\nPress enter to continue")
+    restart_eli()
 
 
 def choice1_2b():
     clear()
-    print()
     print(
-        "As Eli drives on to bridge 2, "
+        "\nAs Eli drives on to bridge 2, "
         "he sees something at the end of the bridge.")
     print(
         "The end of the bridge has been blocked off "
         "by a broken down fuel tanker.")
     print("He jumps out of the car and makes a run for it.")
     print("He is not fast enough and so the Police catch him.")
-    print()
-    print()
-    begin_game()
+    input("\nPress enter to continue")
+    restart_eli()
 
 
 def choice1_3():
     clear()
-    print()
     print(
-        "Eli takes the keys to the boat and runs "
+        "\nEli takes the keys to the boat and runs "
         "to the nearby river where he finds the boat parked "
         "and also a scuba gear with an oxygen tank. ")
     print("All he has to do is to choose either: ")
     print("choice 1:  The boat")
     print("choice 2:  The scuba gear")
-    print()
     while True:
         fifth_choice = input("Which would you like to choose ( 1 / 2 ):\n ")
         if fifth_choice not in ['1', '2']:
@@ -284,6 +240,8 @@ def choice1_3a():
     print("He drowns after swimming for a few minutes.")
     print()
     print()
+    input("Press enter to continue")
+    restart_eli()
 
 
 def choice1_3b():
@@ -296,43 +254,45 @@ def choice1_3b():
     print(
         "As he does not know how to swim, "
         "he ends up in the river and drowns.")
+    input("\nPress enter to continue")
+    restart_eli()
 
 
 def choice2():
     clear()
-    print()
-    print("Eli turns right to a wide path that leads up to a house.")
+    print("\nEli turns right to a wide path that leads up to a house.")
     print("He could either:")
     print("choice 1: Try to break in from the front")
     print("choice 2: Try to break in from the back")
-    print("choice 3: Use the hidden key under the door mat")
-    print()
+    print("choice 3: Use the hidden key under the door mat\n")
     while True:
         sixth_choice = input("Which choice do you want? ( 1 / 2 / 3 ):\n ")
         if sixth_choice not in ['1', '2', '3']:
             print("That is not a valid option, please try again")
         else:
+            clear()
             if sixth_choice == '1':
-                print()
                 print(
                     "He tries to break in from the front, "
                     "but the door is rock solid.")
                 print("The police catch up to him and he is arrested.")
+                input("\nPress enter to continue")
+                restart_eli()
                 break
             elif sixth_choice == '2':
-                print()
                 print(
-                    "He tries to break in from the back "
+                    "\nHe tries to break in from the back "
                     "by using a large rock to break the window.")
                 print(
                     "He climbs into the house to hide, "
                     "but the owner of the house raises an alarm "
                     "which gets the attention of the police near by.")
                 print("Eli surrenders.")
+                input("\nPress enter to continue")
+                restart_eli()
                 break
             else:
-                print()
-                print("He uses the hidden key under the mat.")
+                print("\nHe uses the hidden key under the mat.")
                 print(
                     "He opens the door and makes his way "
                     "into the living room to hide.")
@@ -342,22 +302,19 @@ def choice2():
                     "Eli screams and tries to fight off the dog, "
                     "but the owner of the house calls the police "
                     "and he is arrested")
+                input("\nPress enter to continue")
+                restart_eli()
                 break
 
 
 clear()
-print()
-print()
-print("    #####################")
+print("\n\n    #####################")
 print("    #                   #")
 print("    #  Help Eli Escape  #")
 print("    #                   #")
 print("    #####################")
-print()
-print()
-print("Eli just robbed the Central Bank of the world.")
-print("Eli is trying to escape with large bags of cash.")
-print()
+print("\n\nEli just robbed the Central Bank of the world.")
+print("Eli is trying to escape with large bags of cash.\n")
 while True:
     help_eli = input(
         "Would you like to help Eli escape from the police?"
@@ -371,5 +328,5 @@ while True:
         else:
             clear()
             print("Eli won't be getting any help this time")
-            print("Thanks for playing")
+            print("\nThanks for playing")
             break
